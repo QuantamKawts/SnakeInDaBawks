c44116888cf0406a8b6c0f26be226beaf6f9f938
QuantamKawts <31378485+QuantamKawts@users.noreply.github.com>
2017-09-03 13:00:10 -0400

    Update snakeindabawks.py
    Moved Selenium package from the web framework bawks to the Data Programming Bawks
    Added Pyramid package to the web framework bawks
    Added Pelican packages to the web framework bawks,
    Added another menu option to view a report of all installed packages within the environment

--- snakeindabawks.py
+++ snakeindabawks.py
@@ -3,10 +3,12 @@ from subprocess import Popen
 import os
 from threading import Thread
 from time import sleep
+import datetime
 
 
 
-'''TODO : - Logging - Log export of the installed packages (perhaps to a txt file and include date and time)
+'''TODO :
+
 ##       - Offline site packages zip extract (if no internet connection)
 ##       - Transform the entire terminal interface to a GUI form using tk or wx
 ##       - Rewrite script in python3
@@ -14,7 +16,7 @@ from time import sleep
 
 '''
 print "*********************************************************************"
-print "-------====Snake In Da Bawks ver 1.0.0 - By QuantamKawts====---------"
+print "-------====Snake In Da Bawks ver 1.0.1 - By QuantamKawts====---------"
 print "*********************************************************************"
 print "A terminal based wizard to help python developers quickly develop"
 print" independant project enviornments and install categorized groups of popular pip packages by modules"
@@ -70,6 +72,7 @@ def removebawks():
                 print "Done!!!"
 
 def getProjectName():
+
     print "Please Enter the name of your project"
     ootbprojname = raw_input()
     thread2h = Thread(group=None, target=lambda:Popen(["virtualenv", ootbprojname]))
@@ -78,7 +81,7 @@ def getProjectName():
         print "Almost!"
     else:
         print "Please wait a few seconds while your project is being built..."
-    sleep(30) # it takes this long to build the enviornment 
+    sleep(20) # it takes this long to build the enviornment 
 
     os.chdir(ootbprojname)
     print "You are now in the root of:" +ootbprojname 
@@ -86,6 +89,7 @@ def getProjectName():
     os.chdir("scripts")
     print "You are now in the scripts directory of your project"
     print os.getcwd()
+
 def makerequiredfiles():
 
     file = open("webbawks.txt","w")
@@ -94,17 +98,18 @@ def makerequiredfiles():
     file.write("bottle \r\n")
     file.write("web2py \r\n")
     file.write("requests \r\n")
-    file.write("selenium \r\n")
+    file.write("pyramid \r\n")
+    file.write("pelican \r\n")
     file.close()
 
     file = open("databawks.txt","w")
     file.write("numpy \r\n")
     file.write("pandas \r\n")
     file.write("scrapy \r\n")
+    file.write("selenium \r\n")
     file.write("beautifulsoup \r\n")
     file.write("bokeh \r\n")
     file.write("matplotlib \r\n")
-    file.write("opencv \r\n")
     file.write("arrow \r\n")
     file.write("dask \r\n")
     file.write("pygal \r\n")
@@ -122,6 +127,12 @@ def makerequiredfiles():
     file.write("neurolab \r\n")
     file.write("quepy \r\n")
     file.close()
+
+def whatsindabawks():
+    installedbawkes = open("InstalledBawksesReport.txt", "a")
+    installedbawkes.write("---All the packages installed from SnakeInDaBawks--- \r\n")
+    installedbawkes.write(datetime.datetime.now().ctime())
+    installedbawkes.close()   
     
 def pipPackageMenu():
     menu = {}
@@ -130,15 +141,16 @@ def pipPackageMenu():
     menu['3']= "Install AI Bawks"
     menu['4']= "Install All 1,2,3"
     menu['5']= "Remove All Bawks 1,2,3"
-    menu['6']="Exit"
+    menu['6']="View installed packages from SnakeInDaBawks"
+    menu['7']="Exit"    
     while True: 
         options=menu.keys()
         options.sort()
         print "Please choose what packages you want to install "
         print "Packages will be installed within the project (lib/site-packages)"
-        print " [1] [Web Framework Bawks] - Contains flask, django, bottle, web2py, requests, selenium"
+        print " [1] [Web Framework Bawks] - Contains flask, django, bottle, web2py, requests, pelican, pyramid,"
         print " ----------------------------------------------------------------------------------------------"
-        print " [2] [Data programming Bawks] - Contains numpy, pandas, scrapy, beautifulsoup, bokeh, matplotlib, opencv, arrow, dask, pygal, csvkit, sqlalchemy"
+        print " [2] [Data programming Bawks] - Contains numpy, pandas, scrapy, selenium, beautifulsoup, bokeh, matplotlib, arrow, dask, pygal, csvkit, sqlalchemy"
         print " ----------------------------------------------------------------------------------------------"
         print " [3] [Artifical Intelligence Bawks] Contains - easyai, pybrain, pattern, aima, simpleai, scikit-learn, neurolab, quepy"
         print " ----------------------------------------------------------------------------------------------"
@@ -146,8 +158,10 @@ def pipPackageMenu():
         print " ----------------------------------------------------------------------------------------------"
         print " [5] [Remove all Bawkses 1,2,3]"
         print " ----------------------------------------------------------------------------------------------"
-        print " [6] [EXIT]"         
-        selection=raw_input("Please Select a Bawks:")
+        print " [6] [View all installed packages and version info]"  
+        print " ----------------------------------------------------------------------------------------------"  
+        print " [7] [EXIT]"  
+        selection=raw_input("Please Select a option:")
 
         if selection =='1': 
             thread1a = Thread(group=None, target=lambda:os.system("pip install -r webbawks.txt"))
@@ -180,12 +194,19 @@ def pipPackageMenu():
         elif selection == '5': 
             print "Remove all from 1,2,3 bawkses"
             removebawks()
-        elif selection == '6': 
+
+        elif selection == '6':
+            os.system("pip freeze installedbawks.txt")
+            os.system("pip freeze installedbawks.txt > InstalledBawksesFile.txt" )
+            whatsindabawks()
+
+        elif selection == '7': 
             break            
         else: 
-            print "Invalid Option Selected! Are you Drunk?"
+            print "Invalid Option Selected! Are you Drunk?"            
 
 getvirtualenv()
 getProjectName() #comment out this line if you want to install the lib packages globally
 makerequiredfiles()
 pipPackageMenu()
+
